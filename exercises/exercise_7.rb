8520a7a331e6c4bc031eb7c1e27551aa1bd4013e
Judith Huynh <judithuynh@gmail.com>
2020-05-26 23:15:21 +0000

    validated databases

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,24 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :hourly_rate, numericality: { :greater_than_or_equal_to => 40, :less_than_or_equal_to => 200}
+end
+
+class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3}
+  validates :annual_revenue, numericality: { :greater_than_or_equal_to => 0 }
+end
+
+puts "Enter store name"
+user_input = gets.chomp
+
+store = Store.create(name: "#{user_input}")
+
+store.errors.full_messages.each do |err|
+  puts "#{err} Must have other informaton"
+end
